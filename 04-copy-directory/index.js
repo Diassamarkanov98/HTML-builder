528255a01e5c11fbdc9cafccdb79b0784dd836fb
Dias Samarkanov <77679797+Diassamarkanov98@users.noreply.github.com>
2021-11-09 01:41:58 +0600

    feat: add solution to 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+  
+fs.mkdir(path.join(__dirname, 'files-copy'),
+  { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    getCurrentFilenames();
+    console.log('Directory created successfully!');
+  });
 
+  function getCurrentFilenames() {
+    console.log("\nCurrent filenames:");
+    fs.readdir(`${__dirname}/files`, (err, files) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("\nCurrent directory filenames:");
+          files.forEach(file => {
+            fs.copyFile(`${__dirname}/files/${file}`,`${__dirname}/files-copy/${file}`,(err) => {
+                if (err) {
+                  console.log("Error Found:", err);
+                }
+              });
+            console.log(file);
+          })
+        }
+      })
+  }
