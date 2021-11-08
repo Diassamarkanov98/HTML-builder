4b1109b59655aac2b319180c5d2ace4c9129179d
Dias Samarkanov <77679797+Diassamarkanov98@users.noreply.github.com>
2021-11-09 01:40:29 +0600

    feat: add solution to 1 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+
+let line;
+const streamTxt = fs.createReadStream(path.join(__dirname, 'text.txt'), {encoding:'utf-8'});
+
+streamTxt.on('data', (data) => console.log(data.split('\n')[0]));
+streamTxt.on('error', (err) => console.error(`err: ${err}`));
